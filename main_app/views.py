3862809d8aa68183a06b7ab4bf22dbb4742a6c16
Jordin Bryant <jordintaylor095@gmail.com>
2022-10-21 12:59:42 -0400

    populated dress list

--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,22 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')  
+
+def dressess_index(request):
+#   dresses = Dress.objects.filter(user=request.user)  
+  return render(request, 'dresses/index.html', { 'dresses': dresses })  
+
+
+class Dress:  
+  def __init__(self,style, name, material, description, color):
+    self.style = style
+    self.name = name
+    self.material = material
+    self.description = description
+    self.color = color
+
+dresses = [
+    Dress('Special Occasion', 'Lola', 'Satin', 'trendy cowl neckline with a slim-fit bodice','Taupe'),
+    Dress('Coctail', 'Malika', 'Polyester', 'Simmering sequins to sparkle under the moonlit night. V-neckline','Rose gold'),
+    Dress('Formal', 'Gabriela', 'Velvet', 'Romantic sweetheart neckline', 'Burgundy'),
+]
